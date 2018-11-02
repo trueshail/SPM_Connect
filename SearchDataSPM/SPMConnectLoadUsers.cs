d054c62853436e060ce310c4e930963961e525e0
Shail Patel <shail@spm-automation.com>
2018-11-02 09:15:59 -0400

    Last Space on Load Users

--- SearchDataSPM/SPMConnectLoadUsers.cs
+++ SearchDataSPM/SPMConnectLoadUsers.cs
@@ -124,9 +124,6 @@ namespace SearchDataSPM
                 {
                     cn.Close();
                     return true;
-
-                    
-
                 }
                 else
                 {
