66405c3e43703bc60078da273b16166da9e70c9d
Shail Patel <luv4ever2shail@gmail.com>
2020-07-07 15:20:13 -0400

    Added Functionality to display inventory data and updated method to capture stored proc. Added right click menu items for duplicates and inventory

--- SearchDataSPM/Properties/Resources.Designer.cs
+++ SearchDataSPM/Properties/Resources.Designer.cs
@@ -63,9 +63,29 @@ namespace SearchDataSPM.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap icons8_clone_32 {
+            get {
+                object obj = ResourceManager.GetObject("icons8_clone_32", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap icons8_Report_Card_64 {
             get {
                 object obj = ResourceManager.GetObject("icons8_Report_Card_64", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icons8_warehouse_32 {
+            get {
+                object obj = ResourceManager.GetObject("icons8_warehouse_32", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
